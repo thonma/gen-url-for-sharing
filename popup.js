348702a578dc9ef84fd14a9984c659e2b15b7812
takumi.honma <takumi.honma@souken.co.jp>
2020-12-23 12:51:39 +0900

    FEAT フォーマット変更機能

--- popup.js
+++ popup.js
@@ -1,35 +1,58 @@
-const queryInfo = {
-  'active': true,
-  'lastFocusedWindow': true
-};
-chrome.tabs.query(queryInfo, tabs => {
-  // ==================================
-  // コピペ用フォーマット
-  // ==================================
-  const FMT = `■{{title}}
+const FMT_CONFIG_KEY = 'FMT_CONFIG';
+const INITIAL_FMT = `■{{title}}
 {{url}}`;
 
-  // ==================================
-  // <title> のテキストと URL の取得
-  // ==================================
-  const title = tabs[0].title;
-  const url = tabs[0].url;
+window.onload = () => {
+  document.querySelector('#Fmt').addEventListener('change', () => {
+    // ==================================
+    // 設定保存
+    // ==================================
+    chrome.storage.sync.set({
+      [FMT_CONFIG_KEY]: document.querySelector('#Fmt').value,
+    });
+  });
+};
 
-  // ==================================
-  // コピペ用に整形
-  // ==================================
-  const formattedText = FMT.replace('{{title}}', title).replace('{{url}}', url);
+chrome.storage.sync.get([FMT_CONFIG_KEY], items => {
+  const fmt = items[FMT_CONFIG_KEY] || INITIAL_FMT;
+  document.querySelector('#Fmt').textContent = fmt;
+  document.querySelector('#Fmt').value = fmt;
 
-  // ==================================
-  // コピー
-  // Clipboard API はデフォルト無効のため document.execCommand('copy') を使う
-  // document.execCommand('copy') を使うために一時的に <textarea> を描画する
-  // ==================================
-  const tmpElement = document.createElement('textarea');
-  tmpElement.textContent = formattedText;
-  document.body.appendChild(tmpElement);
-  tmpElement.select();
-  document.execCommand('copy');
-  tmpElement.blur();
-  document.body.removeChild(tmpElement);
+  const queryInfo = {
+    'active': true,
+    'lastFocusedWindow': true
+  };
+  chrome.tabs.query(queryInfo, tabs => {
+    if (tabs.length === 0) return;
+
+    // ==================================
+    // <title> のテキストと URL の取得
+    // ==================================
+    const title = tabs[0].title;
+    const url = tabs[0].url;
+
+    // ==================================
+    // コピペ用に整形
+    // ==================================
+    const formattedText = fmt.replace('{{title}}', title).replace('{{url}}', url);
+
+    // ==================================
+    // コピー
+    // Clipboard API はデフォルト無効のため document.execCommand('copy') を使う
+    // document.execCommand('copy') を使うために一時的に <textarea> を描画する
+    // ==================================
+    const tmpElement = document.createElement('textarea');
+    tmpElement.textContent = formattedText;
+    document.body.appendChild(tmpElement);
+    tmpElement.select();
+    document.execCommand('copy');
+    tmpElement.blur();
+    document.body.removeChild(tmpElement);
+
+    // ==================================
+    // コピーしたテキストを表示
+    // ==================================
+    document.querySelector('#CopiedTxt').textContent = formattedText;
+    document.querySelector('#Notice').removeAttribute('hidden');
+  });
 });
